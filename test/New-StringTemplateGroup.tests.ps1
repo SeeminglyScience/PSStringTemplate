c4b558d72b104cbde14819a2c6651ae42d7e9943
Patrick M. Meinecke <seeminglyscience@gmail.com>
2017-05-15 21:40:29 -0400

    Fix parse exception context messages
    This fixes several issues where the context message in parse exceptions
    could be blank, highlight the wrong character, or show the wrong line.
    
    - Added tests for validating compile time parse exceptions.
    
    - Added a VSCode debug configuration for current file in PowerShell.
    
    - Update version number and release notes.

--- test/New-StringTemplateGroup.tests.ps1
+++ test/New-StringTemplateGroup.tests.ps1
@@ -29,3 +29,59 @@ template1() ::= <<
         $result | Invoke-StringTemplate | Should Be "    This is the first line`r`n    this is the second"
     }
 }
+Describe 'Group exception handling tests' {
+    # Verifies the parse message displays the correct code, highlights the right
+    # character, and relays the correct message from the engine.
+    function ShouldThrowParse {
+        [CmdletBinding()]
+        param(
+            [string]$ContextMessage,
+            [string]$Body,
+            [single]$Offset = 0,
+            [Parameter(ValueFromPipeline)][scriptblock]$InputObject
+        )
+        process {
+            $exceptionString = '(' +[regex]::Escape($ContextMessage) + ')' +
+                               ".*\+\s{$($ContextMessage.Length + $Offset)}~"
+            if ($Body) {
+                $exceptionString += '.*{0}' -f $Body
+            }
+
+            try {
+                $null = & $InputObject
+            } catch {
+                $actualMessage = $_.Exception.Message
+            }
+            if ($actualMessage) {
+                $actualMessage | Should Match ('(?s)' + $exceptionString)
+            }
+        }
+    }
+    It 'displays TemplateGroupCompiletimeMessage' {
+        $definition = 'a()::='
+        { New-StringTemplateGroup -Definition $definition } |
+            ShouldThrowParse $definition -Body 'missing template at ''\<EOF\>' -Offset 1
+    }
+    It 'displays TemplateLexerMessage' {
+        $definition = 'a(x)::= "<x; separator=\",>"'
+        { New-StringTemplateGroup -Definition $definition } |
+            ShouldThrowParse '<x; separator=",>' -Body 'EOF in string' -Offset 1
+    }
+    It 'displays TemplateLexerMessage with multiple lines' {
+        $definition = '
+        a(x)::= "<x>"
+        b(x)::= "<x; separator=\",>"
+        c(x)::= "<x>'
+        { New-StringTemplateGroup -Definition $definition } |
+            ShouldThrowParse '<x; separator=",>' -Body 'EOF in string' -Offset 1
+    }
+    It 'displays TemplateCompiletimeMessage' {
+        { New-StringTemplateGroup -Definition 'a(x)::= "<x"' } |
+            ShouldThrowParse '<x' -Body 'premature EOF' -Offset 1
+    }
+    It 'highlights the right character mid context' {
+        { New-StringTemplateGroup -Definition 'a(x)::= "<sep;;>"' } |
+            ShouldThrowParse '<sep;;>' -Body 'mismatched input '';'' expecting ID' -Offset -1
+
+    }
+}
