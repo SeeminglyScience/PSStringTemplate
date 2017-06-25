291c90d9d723d22ada4a14fd5e7e13eb58b5e468
Patrick M. Meinecke <seeminglyscience@gmail.com>
2017-06-24 20:43:40 -0400

    Fixes to PSObject handling
    - Fix an issue where attempting to access a missing property on a
      PSObject would throw an exception.
    
    - Fix an issue where if statements would not count empty strings and
      arrays as false.

--- src/PSStringTemplate/PSObjectAdaptor.cs
+++ src/PSStringTemplate/PSObjectAdaptor.cs
@@ -8,25 +8,37 @@ namespace PSStringTemplate
     class PSObjectAdaptor : ObjectModelAdaptor
     {
         public override object GetProperty(Interpreter interpreter,
-                                            TemplateFrame frame,
-                                            object obj,
-                                            object property,
-                                            string propertyName)
+                                           TemplateFrame frame,
+                                           object obj,
+                                           object property,
+                                           string propertyName)
         {
             if (obj is PSObject psObject)
             {
-                return psObject
-                           .Properties
-                           .First(p => p.Name == propertyName)
-                           .Value
-                       ?? psObject.Methods.First(
-                           m => m.Name == string.Concat("Get", propertyName) &&
-                                m.OverloadDefinitions.First().Contains(@"()") &&
-                                !m.OverloadDefinitions.First().Contains("void")).Invoke();
+                var result = psObject.Properties.FirstOrDefault(p => p.Name == propertyName);
 
+                if (result != null) return ProcessValue(result.Value);
+
+                var method = psObject.Methods.FirstOrDefault(
+                    m => m.Name == string.Concat("Get", propertyName) &&
+                         m.OverloadDefinitions.FirstOrDefault().Contains(@"()") &&
+                        !m.OverloadDefinitions.FirstOrDefault().Contains("void"));
+                
+                return method != null
+                    ? ProcessValue(method?.Invoke())
+                    : null;
             }
 
             return base.GetProperty(interpreter, frame, obj, property, propertyName);
+        }
+        private static object ProcessValue(object value)
+        {
+            // Enable treating null-like values like empty strings and arrays from PSObject as null.
+            return value == null
+                ? null
+                : LanguagePrimitives.IsTrue(value)
+                    ? value
+                    : null;
         }
     }
 }
